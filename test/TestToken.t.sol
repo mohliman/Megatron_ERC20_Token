53330ad4413bf1921e109ef0de63e45324c8faff
Muhammad Baqir Abubakar <bankitliman@gmail.com>
2025-08-05 09:36:07 +0100

    modules

---
+++ test/TestToken.t.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Test} from "forge-std/Test.sol";
+import {DeployToken} from "../script/DeployToken.s.sol";
+import {ERC20Token} from "../src/ERC20Token.sol";
+
+contract TestToken is Test{
+
+}
